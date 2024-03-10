3196cd5087cd79661e291bfc74f85c32b6bdcb5c
salesni <fuensal98@gmail.com>
2024-03-10 14:40:15 -0600

    Finish Form stylung

--- src/components/Reservation/BookingForm.js
+++ src/components/Reservation/BookingForm.js
@@ -1,4 +1,5 @@
 import React, { useState, useReducer } from 'react';
+import Button from '../commons/Button/Button';
 
 const SELECT_TIME = 'SELECT_TIME';
 const UPDATE_TIMES = 'UPDATE_TIMES';
@@ -36,6 +37,9 @@ function BookingForm() {
   const [time, setTime] = useReducer(selectTime, { selectedTime: '17:00' });
   const [guests, setGuests] = useState('');
   const [ocasion, setOcasion] = useState('');
+  const [name,setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
 
   
   const [availableTimes, dispatchTimes] = useReducer(updateTimes, initializeTimes());
@@ -47,35 +51,75 @@ function BookingForm() {
 
   return (
     <section id='BookingForm'>
-      <div  id='menuHeaders'>
+      <div>
         <h1>Book Now</h1>
       </div>
       <hr/>
       <form>
-        <label htmlFor="res-date">Choose date</label>
-        <input type="date" id="res-date" value={date} onChange={handleDateChange} />
-        <label htmlFor="res-time">Choose time</label>
-        <select
-          id="res-time"
-          onChange={(event) =>
-            setTime({ type: SELECT_TIME, times: event.target.value })
-          }
-          value={time.selectedTime}
-        >
-          {availableTimes.times.map((timeOption) => (
-            <option key={timeOption} value={timeOption}>
-              {timeOption}
-            </option>
-          ))}
-        </select>
-        <label htmlFor="guests">Number of guests</label>
-        <input type="number" placeholder="1" min="1" max="10" id="guests" />
-        <label htmlFor="occasion">Occasion</label>
-        <select id="occasion">
-          <option>Birthday</option>
-          <option>Anniversary</option>
-        </select>
-        <input type="submit" value="Make Your reservation" />
+        <div className='formRow'>
+          <label htmlFor="res-name">Full Name:</label>
+          <input type='text'id='res-name'name='name'value={name} required
+              placeholder='Enter your name'
+            />
+        </div>
+        <div className='formRow'>
+          <label htmlFor='res-email'>Email Address:</label>
+          <input type='email'id='res-email'name='email'value={email} required
+              placeholder='Enter your email address'
+            />
+        </div>
+        <div className='formRow'>
+          <label for="res-phone">Phone Number:</label>
+          <input type="tel" id="res-phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
+          placeholder="123-456-7890" required/>
+        </div>
+        <div className='formRow'>
+          <label htmlFor="guests">Number of guests:</label>
+          <div className='incrementGuestsContainer'>
+                <button onClick={()=>(-1)}>-</button>
+                <input type="number" id='guests'/>
+                <button onClick={()=>(1)}>+</button>
+            </div>
+        </div>
+        <div className='formRow'>
+          <div className='dateInputs'>
+            <div>
+              <label htmlFor="res-date">Choose date:</label>
+              <input type="date" id="res-date" value={date} onChange={handleDateChange} />
+            </div>
+            <div>
+              <label htmlFor="res-time">Choose time:</label>
+              <select
+                id="res-time"
+                onChange={(event) =>
+                  setTime({ type: SELECT_TIME, times: event.target.value })
+                }
+                value={time.selectedTime}
+              >
+                {availableTimes.times.map((timeOption) => (
+                  <option key={timeOption} value={timeOption}>
+                    {timeOption}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+        </div>
+        <div className='formRow'>
+          <label htmlFor="occasion">Occasion:</label>
+          <select id="occasion">
+            <option>None</option>
+            <option>Birthday</option>
+            <option>Anniversary</option>
+          </select>
+        </div>
+        <div className='buttonContainer'>
+          <Button title={'Cancel'} 
+          colorClass='red'/>
+          <Button title={'Reserve a Table'} 
+          colorClass=''/>
+
+        </div>
       </form>
     </section>
   );
--- src/components/Reservation/Reservation.js
+++ src/components/Reservation/Reservation.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../Header';
 import Footer from '../Footer/Footer';
 import BookingForm from './BookingForm';
-
+import './Reservation.css'
 function Reservation() {
   return (
     <>
