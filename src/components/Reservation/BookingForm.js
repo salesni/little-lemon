490b844582fc8e90403c89c4844880af39e21aa7
salesni <fuensal98@gmail.com>
2024-03-18 00:04:29 -0600

    Ass states to all variables inside the form

--- src/components/Reservation/BookingForm.js
+++ src/components/Reservation/BookingForm.js
@@ -9,10 +9,12 @@ function BookingForm() {
   const [date, setDate] = useState(new Date());
   const [time, setTime] = useState('17:00');
   const [guests, setGuests] = useState('');
-  const [ocasion, setOcasion] = useState('');
+  const [occasion, setOccasion] = useState('');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [isValidEmailState, setIsValidEmailState] = useState(true); 
   const [phone, setPhone] = useState('');
+  const [isValidPhone, setIsValidPhone] = useState(true); 
   const [availableTimes, setAvailableTimes] = useState([]);
 
   const fetchTimeWithDate = async (date) => {
@@ -41,6 +43,46 @@ function BookingForm() {
     dateUpdated(event.target.value);
   };
 
+
+  const handleEmailChange = (event) => {
+    const email = event.target.value;
+    setEmail(email);
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValidEmail = emailRegex.test(email);
+    setIsValidEmailState(isValidEmail);
+  };
+
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handlePhoneChange = (event) =>{
+    const phone = event.target.value;
+    setPhone(phone);
+
+    const phoneRegex = /^[0-9]{3}-[0-9]{3}-[0-9]{4}$/;
+    const isValidPhone = phoneRegex.test(phone);
+    setIsValidPhone(isValidPhone);
+  }
+
+  const increaseDecreaseGuests = (input) =>{
+    const newNumberOfGuests  = isNaN(guests)? input : parseInt(guests) + input;
+    newNumberOfGuests < 0 ? setGuests(0):setGuests(newNumberOfGuests);
+  }
+
+  const guestChanged = (event) =>{
+    let num = event.target.value;
+    if (num === '') {
+      setGuests('');
+    } else {
+      num = parseInt(num.replace(/^0+/, ''));
+      setGuests(num);
+    }
+  }
+
+
+
   return (
     <section id='BookingForm'>
       <div>
@@ -52,25 +94,29 @@ function BookingForm() {
           <label htmlFor="res-name">Full Name:</label>
           <input type='text'id='res-name'name='name'value={name} required
               placeholder='Enter your name'
+              onChange={handleNameChange}
             />
         </div>
         <div className='formRow'>
           <label htmlFor='res-email'>Email Address:</label>
           <input type='email'id='res-email'name='email'value={email} required
               placeholder='Enter your email address'
+              onChange={handleEmailChange}
             />
         </div>
         <div className='formRow'>
           <label for="res-phone">Phone Number:</label>
           <input type="tel" id="res-phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" 
-          placeholder="123-456-7890" required/>
+          placeholder="123-456-7890" value={phone} onChange={handlePhoneChange} required/>
         </div>
         <div className='formRow'>
           <label htmlFor="guests">Number of guests:</label>
           <div className='incrementGuestsContainer'>
-                <button onClick={()=>(-1)}>-</button>
-                <input type="number" id='guests'/>
-                <button onClick={()=>(1)}>+</button>
+                <button onClick={()=>increaseDecreaseGuests(-1)}>-</button>
+                <input value={(guests === '' ) ? 0 : parseInt(guests)}
+                 onChange={guestChanged}
+                 type="number" id='guests'/>
+                <button onClick={()=>increaseDecreaseGuests(1)}>+</button>
             </div>
         </div>
         <div className='formRow'>
@@ -97,7 +143,7 @@ function BookingForm() {
         </div>
         <div className='formRow'>
           <label htmlFor="occasion">Occasion:</label>
-          <select id="occasion">
+          <select id="occasion" value={occasion} onChange={(event) =>setOccasion(event.target.value)}>
             <option>None</option>
             <option>Birthday</option>
             <option>Anniversary</option>
